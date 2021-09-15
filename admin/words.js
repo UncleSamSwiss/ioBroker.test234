6147b300c834a185b1c523c41881726711754111
ioBroker Adapter Creator <noreply@iobroker.net>
2021-09-15 20:39:06 +0200

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"test234 adapter settings": {
+		"en": "Adapter settings for test234",
+		"de": "Adaptereinstellungen für test234",
+		"ru": "Настройки адаптера для test234",
+		"pt": "Configurações do adaptador para test234",
+		"nl": "Adapterinstellingen voor test234",
+		"fr": "Paramètres d'adaptateur pour test234",
+		"it": "Impostazioni dell'adattatore per test234",
+		"es": "Ajustes del adaptador para test234",
+		"pl": "Ustawienia adaptera dla test234",
+		"zh-cn": "test234的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
